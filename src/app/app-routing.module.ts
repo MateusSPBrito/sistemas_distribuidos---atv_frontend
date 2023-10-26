e7652e49600f6a08c72efe4f4488d5cb1b508e55
MateusSPBrito <mateus.s.p.brito2001@gmail.com>
2023-10-25 21:44:09 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './pages/posts/posts.component';
+import { NewPostComponent } from './pages/new-post/new-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', component: PostsComponent },
+  { path: 'new-post', component: NewPostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { NewPostComponent } from './pages/new-post/new-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
